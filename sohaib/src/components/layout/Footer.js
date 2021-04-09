85ff1a1223b33cf5ae03a26003795daed73ab5aa
shaiksohaib <sm.sohaib01@gmail.com>
2021-04-09 22:15:24 +0530

    V2

--- sohaib/src/components/layout/Footer.js
+++ sohaib/src/components/layout/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
       <div className="row">
         <div className="col-md-12">
           <div className="footer p-3 mt-4 text-center bg-dark text-light">
-            Developed By:
+            Developed By: 
             <span className="text-warning font-weight-normal">
-              Yaser AlAzem
+               SHAIK SOHAIB
             </span>
             , Using <i className="fab fa-react" /> React JS &amp; Redux JS
             integrated with external movies data API
