9f38f2f9d46db939b304bec7008fa14d2dad8b96
shaiksohaib <sm.sohaib01@gmail.com>
2021-04-09 22:15:09 +0530

    V2

--- sohaib/src/components/home/SearchForm.js
+++ sohaib/src/components/home/SearchForm.js
@@ -21,17 +21,17 @@ export class SearchForm extends Component {
 
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid mt-5 text-center">
+      <div className="jumbotron jumbotron-fluid mt-5 text-center  bg-dark">
         <div className="container">
-          <h1 className="display-4 mb-3">
-            <i className="fa fa-search" /> Search for a movie ,TV series ..
+          <h1 className="display-4 mb-3 text-light ">
+            <i className="" />  Search for content
           </h1>
           <form id="searchForm" onSubmit={this.onSubmit}>
             <input
               type="text"
               className="form-control"
               name="searchText"
-              placeholder="Search Movies, TV Series ..."
+              placeholder="Search Movies, TV Series .."
               onChange={this.onChange}
             />
             <button type="submit" className="btn btn-primary btn-bg mt-3">
